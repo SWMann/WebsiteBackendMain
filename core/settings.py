fd0f403e0d7e9cb920550c89310fdac27febf905
SWMann <schaeferwmann@gmail.com>
2025-05-09 19:53:21 +0100

    Uploading static

--- core/settings.py
+++ core/settings.py
@@ -117,11 +117,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'websitebackendmain/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
-MEDIA_URL = 'websitebackendmain/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
