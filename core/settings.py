508c518318341207d4b649aeba39a1799f3ce04d
SWMann <schaeferwmann@gmail.com>
2025-05-09 19:11:11 +0100

    Fix to settings

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/websitebackendmain/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 # Media files
 MEDIA_URL = '/websitebackendmain/media/'
