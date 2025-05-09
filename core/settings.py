14da267b495ce45ee7ae642964a9f778b0367116
SWMann <schaeferwmann@gmail.com>
2025-05-09 18:34:47 +0100

    Fix to settings

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent+"/websitebackendmain/"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key-for-development-only')
