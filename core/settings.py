5dac4b2959f60c5dfc4b43c8f31a2862d437064d
SWMann <schaeferwmann@gmail.com>
2025-05-09 18:53:18 +0100

    Fix to settings

--- core/settings.py
+++ core/settings.py
@@ -117,12 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/websitebackendmain/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/websitebackendmain/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Media files
 MEDIA_URL = '/websitebackendmain/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/websitebackendmain/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
